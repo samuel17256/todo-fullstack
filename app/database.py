cd0db8c4e6849e047bebd195d651cde6d5a6f4cd
Samuel Nanbam Luka <samuelnanbam@gmail.com>
2026-05-20 00:04:04 +0000

    feat:modify config file in backend

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
-engine = create_engine(settings.database_url, pool_pre_ping=True)
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
